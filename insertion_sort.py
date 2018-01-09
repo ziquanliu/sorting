52357dd429467aebf04cbc0f6aa3e41b5bbc3506
ziquanliu <ziquanliu2-c@my.cityu.edu.hk>
2018-01-10 00:06:36 +0800

    complete insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,4 +1,21 @@
-
+import random
 
 def insert_sorting(A):
     l=len(A)
+    i=1
+    while i<l:
+        key=A[i]
+        j=i-1
+        while j>=0 and A[j]>key:
+            A[j+1]=A[j]
+            j=j-1
+        A[j+1]=key
+        i+=1
+    return A
+
+l=[]
+for i in range(20):
+    l.append(random.randint(0,99))
+
+l_s=insert_sorting(l)
+print l_s
